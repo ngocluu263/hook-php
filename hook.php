8806b17a3bffab672582a139a767a8779f33e42f
Luu Nguyen <luunguyen@ccintegration.com>
2016-09-29 22:18:18 +0700

    Update

--- hook.php
+++ hook.php
@@ -2,12 +2,16 @@
 // set autoload
 require 'vendor/autoload.php';
 
-#use Guzzle\Http\Client;
 
+//$hook = Hook\Client::configure(array(
+//  'app_id' => 1,
+//  'key' => '638d9ba9643e568c1065084e3e67fb7e',
+//  'endpoint' => 'http://192.168.116.135:4665/index.php/'
+//));
 $hook = Hook\Client::configure(array(
   'app_id' => 1,
-  'key' => '638d9ba9643e568c1065084e3e67fb7e',
-  'endpoint' => 'http://172.16.4.56:4665/index.php/'
+  'key' => '57ec549c9f303e0a188b4567',
+  'endpoint' => 'http://192.168.116.135:4665/index.php/'
 ));
 
 $hook->collection('scores')->create(array(
